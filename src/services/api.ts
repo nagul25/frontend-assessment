81071654b7a1ac3097266e9c2a2153929a03d7dc
Aravind <aravind.babu@numentica-ui.com>
2025-12-05 15:29:45 +0530

    Added threadId in api call

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,8 +12,13 @@ const apiClient = axios.create({
 
 export const chatService = {
   async sendMessage(request: ChatRequest): Promise<QueryRequestResponseType> {
+    const threadId = sessionStorage.getItem('threadId');
     const formData = new FormData();
     formData.append("prompt", request.message);
+
+    if(threadId) {
+      formData.append("thread_id", threadId || "");
+    }
 
     if (request.files && request.files.length > 0) {
       request.files.forEach((file) => {
@@ -32,6 +37,9 @@ export const chatService = {
       });
       
       console.log("API Response:", response.data.data);
+      if(!threadId) {
+        sessionStorage.setItem('threadId', response.data.data.thread_id);
+      }
       return response.data?.data;
     } catch (error) {
       console.error("Error sending message:", error);
